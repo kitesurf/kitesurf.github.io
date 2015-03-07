46486deb4dfa9a9494048906559137493830cec5
kitesurf <kitesurfdaniel@gmail.com>
2015-03-07 09:19:30 +0100

    added javascript for responsive images and lazyload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,84 +1,117 @@
 'use strict';
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
+grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+    responsive_images: {
+      dev: {
+        options: {},
+        sizes: [{
+          width: 320,
+          height: 240
+        },{
+          name: 'large',
+          width: 640
+        },{
+          name: "large",
+          width: 1024,
+          suffix: "_x2",
+          quality: 0.6
+        }],
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: '{,*/}*.{png,jpg,jpeg}',
+          dest: '_dist/'
+      }]
+    }
+  },
+  copy: {
+    dev: {
+      files: [{
+        expand: true,
+        src: ['**/*', '!images/**/*.*'],
+        cwd: 'src/',
+        dest: 'dist/'
+      }]
+    }
+  },
+  jshint: {
+    options: {
+      jshintrc: '.jshintrc'
+    },
+    all: [
+      'Gruntfile.js',
+      'js/*.js',
+      '!js/main.js'
+    ]
+  },
+  watch: {
+    js: {
+      files: [
+        '<%= jshint.all %>'
+      ],
+      tasks: ['jshint', 'uglify'],
       options: {
-        jshintrc: '.jshintrc'
+        livereload: true
+      }
+    },
+  },
+  uglify: {
+    dist: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */',
+        compress: true,
+        beautify: false,
+        mangle: false
       },
-      all: [
-        'Gruntfile.js',
-        'js/*.js',
-        '!js/main.js'
-      ]
-    },
-    watch: {
-      js: {
-        files: [
-          '<%= jshint.all %>'
-        ],
-        tasks: ['jshint', 'uglify'],
-        options: {
-          livereload: true
-        }
+      files: {
+        'js/main.js': [
+          'js/plugins/*.js',
+          'js/_*.js'
+        ]
+      }
+    }
+  },
+  imagemin: {
+    dist: {
+      options: {
+        optimizationLevel: 7,
+        progressive: true
       },
-    },
-    uglify: {
-      dist: {
-        options: {
-          banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */',
-          compress: true,
-          beautify: false,
-          mangle: false
-        },
-        files: {
-          'js/main.js': [
-            'js/plugins/*.js',
-            'js/_*.js'
-          ]
-        }
-      }
-    },
-    imagemin: {
-      dist: {
-        options: {
-          optimizationLevel: 7,
-          progressive: true
-        },
-        files: [{
-          expand: true,
-          cwd: 'images/',
-          src: '{,*/}*.{png,jpg,jpeg}',
-          dest: 'images/'
-        }]
-      }
-    },
-    imgcompress: {
-      dist: {
-        options: {
-          optimizationLevel: 7,
-          progressive: true
-        },
-        files: [{
-          expand: true,
-          cwd: 'images/',
-          src: '{,*/}*.{png,jpg,jpeg}',
-          dest: 'images/'
-        }]
-      }
-    },
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'images/',
-          src: '{,*/}*.svg',
-          dest: 'images/'
-        }]
-      }
-    },
-  });
+      files: [{
+        expand: true,
+        cwd: 'images/',
+        src: '{,*/}*.{png,jpg,jpeg}',
+        dest: 'images/'
+      }]
+    }
+  },
+  imgcompress: {
+    dist: {
+      options: {
+        optimizationLevel: 7,
+        progressive: true
+      },
+      files: [{
+        expand: true,
+        cwd: 'images/',
+        src: '{,*/}*.{png,jpg,jpeg}',
+        dest: 'images/'
+      }]
+    }
+  },
+  svgmin: {
+    dist: {
+      files: [{
+        expand: true,
+        cwd: 'images/',
+        src: '{,*/}*.svg',
+        dest: 'images/'
+      }]
+    }
+  },
+});
 
   // Load tasks
   grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -88,8 +121,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-svgmin');
   grunt.loadNpmTasks('grunt-imgcompress');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-responsive-images');
 
   // Register tasks
+  grunt.registerTask('default', ['responsive_images']);
   grunt.registerTask('scripts', ['watch', 'uglify']);
   grunt.registerTask('images', ['newer:imgcompress', 'newer:svgmin']);
 };
