4d2dbc915c20b8a6285ccca2d96a828ab4a2a8fd
kitesurf <kitesurfdaniel@gmail.com>
2016-11-30 20:18:22 +0100

    changes for inline css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,131 +1,35 @@
-'use strict';
 module.exports = function(grunt) {
+  grunt.initConfig({
+    uncss: {
+      dist: {
+        files: {
+          '_dist/css/tidy.css': ['_site/index.html', '_site/de/index.html']
+        }
+      }
+    },
 
-grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json'),
-    responsive_images: {
-      dev: {
-        options: {},
-        sizes: [{
-          width: 320,
-          height: 240
-        },{
-          name: 'large',
-          width: 640
-        },{
-          name: "large",
-          width: 1024,
-          suffix: "_x2",
-          quality: 0.6
-        }],
+    cssmin: {
+      dist: {
+        options: {
+          compatibility: 'ie8',
+          keepSpecialComments: 0
+        },
         files: [{
           expand: true,
-          cwd: 'images/',
-          src: '{,*/}*.{png,jpg,jpeg}',
-          dest: '_dist/'
-      }]
-    }
-  },
-  copy: {
-    dev: {
-      files: [{
-        expand: true,
-        src: ['**/*', '!images/**/*.*'],
-        cwd: 'src/',
-        dest: 'dist/'
-      }]
-    }
-  },
-  jshint: {
-    options: {
-      jshintrc: '.jshintrc'
-    },
-    all: [
-      'Gruntfile.js',
-      'js/*.js',
-      '!js/main.js'
-    ]
-  },
-  watch: {
-    js: {
-      files: [
-        '<%= jshint.all %>'
-      ],
-      tasks: ['jshint', 'uglify'],
-      options: {
-        livereload: true
-      }
-    },
-  },
-  uglify: {
-    dist: {
-      options: {
-        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */',
-        compress: true,
-        beautify: false,
-        mangle: false
-      },
-      files: {
-        'js/main.js': [
-          'js/plugins/*.js',
-          'js/_*.js'
-        ]
+          cwd: '_dist/css',
+          src: ['*.css', '!*.min.css'],
+          dest: '_dist/css',
+          ext: '.min.css'
+        }]
       }
     }
-  },
-  imagemin: {
-    dist: {
-      options: {
-        optimizationLevel: 7,
-        progressive: true
-      },
-      files: [{
-        expand: true,
-        cwd: 'images/',
-        src: '{,*/}*.{png,jpg,jpeg}',
-        dest: 'images/'
-      }]
-    }
-  },
-  imgcompress: {
-    dist: {
-      options: {
-        optimizationLevel: 7,
-        progressive: true
-      },
-      files: [{
-        expand: true,
-        cwd: 'images/',
-        src: '{,*/}*.{png,jpg,jpeg}',
-        dest: 'images/'
-      }]
-    }
-  },
-  svgmin: {
-    dist: {
-      files: [{
-        expand: true,
-        cwd: 'images/',
-        src: '{,*/}*.svg',
-        dest: 'images/'
-      }]
-    }
-  },
-});
-
-  // Load tasks
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-newer');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-svgmin');
-  grunt.loadNpmTasks('grunt-imgcompress');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-responsive-images');
-
-  // Register tasks
-  grunt.registerTask('default', ['responsive_images']);
-  grunt.registerTask('scripts', ['watch', 'uglify']);
-  grunt.registerTask('images', ['newer:imgcompress', 'newer:svgmin']);
-};
+ })
+    grunt.loadNpmTasks('grunt-uncss');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+ 
+  grunt.registerTask('default', [
+    'uncss',
+    'cssmin'
+    ]);
+ 
+}
