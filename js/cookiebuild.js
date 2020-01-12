fbb627c60faf3af112ba56bfca5a5714de0aa31a
kitesurf <kitesurfdaniel@gmail.com>
2020-01-12 23:19:44 +0100

    cookieconsent anders platziert, sodass eine Navigation ohne löschen möglich ist

--- js/cookiebuild.js
+++ js/cookiebuild.js
@@ -1,19 +1,16 @@
-<script src="{{ site.url }}/js/cookieconsent.min.js" data-cfasync="false"></script>
-<script>
     window.cookieconsent.initialise({
       "palette": {
         "popup": {
-          "background": "#2799d1",
-          "text": "#3e4141"
-        },
-        "button": {
-          "background": "#df061c",
-          "text": "#ffffff"
-        }
-      },
+      "background": "#2799d1",
+      "text": "#3e4141"
+    },
+    "button": {
+      "background": "#df061c",
+      "text": "#ffffff"
+    }
+  },
       "theme": "classic",
-      "position": "bottom-left",
-      "type": "opt-out",
+      "position": "top",
       "content": {
         "message": "{{ lang.cookie_message }}",
         "dismiss": "{{ lang.cookie_dismiss }}",
@@ -21,5 +18,6 @@
         "link": "{{ lang.cookie_link }}",
         "href": "{{ lang.cookie_href }}"
       }
-});
-</script>
+    });
+
+
